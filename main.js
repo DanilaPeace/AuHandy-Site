3b5715a7418295efbf119fab363d7a3034d4f653
Danila <peace_are_off@bk.ru>
2021-09-30 20:14:08 +0300

    The fly window is competed without adaptive

--- main.js
+++ main.js
@@ -10,3 +10,17 @@ menuBtn.addEventListener('click', () => {
         menuOpen = false;
     }
 });
+
+// popup
+const openPopup = document.querySelector(".open_popup");
+const closePopup = document.querySelector(".popup__close");
+const popup = document.querySelector(".popup");
+
+openPopup.addEventListener('click', (event) => {
+    event.preventDefault();
+    popup.classList.add('active');
+});
+
+closePopup.addEventListener('click', function(event){
+    popup.classList.remove('active');
+});
