18280272eec2dc126930cf7d0901617a3c1f0d0f
Danila <peace_are_off@bk.ru>
2021-09-30 22:28:56 +0300

    I added accordions to footer

--- main.js
+++ main.js
@@ -24,3 +24,46 @@ openPopup.addEventListener('click', (event) => {
 closePopup.addEventListener('click', function(event){
     popup.classList.remove('active');
 });
+
+// accordion
+document.addEventListener('DOMContentLoaded', () => {
+
+    const closeAccordion = (accordion, contentSelector) => {
+        const content = accordion.querySelector(contentSelector);
+        accordion.dataset.open = false;
+        content.style.maxHeight = '';
+    }
+
+    const smothHeight = (accordionSelector, titleSelector, contentSelector) => {
+        const accordions = document.querySelectorAll(accordionSelector);
+
+        if (!accordions.length) return;
+
+        accordions.forEach(accordion => {
+            const title = accordion.querySelector(titleSelector);
+            const content = accordion.querySelector(contentSelector);
+
+            title.addEventListener('click', (event) => {
+                if (accordion.dataset.open !== 'true') {
+                    accordions.forEach((accordionForClose) => closeAccordion(accordionForClose, contentSelector));
+                    accordion.dataset.open = true;
+                    content.style.maxHeight = `${content.scrollHeight}px`;
+                } else {
+                    closeAccordion(accordion, contentSelector);
+                }
+            });
+
+            const onResize = () => {
+                if (accordion.dataset.open === 'true') {
+                    if (parseInt(content.style.maxHeight) !== content.scrollHeight) {
+                        content.style.maxHeight = `${content.scrollHeight}px`;
+                    }
+                }
+            };
+
+            window.addEventListener('resize', onResize);
+        })
+    };
+
+    smothHeight('.accordion', '.accordion__title', '.accordion__content');
+});
